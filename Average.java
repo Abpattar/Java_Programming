74d9972acaf495128b4e315c4fc081c67eb9bb0b
Abpattar <adityabpattar@gmail.com>
2025-03-12 23:06:47 +0530

    Added Files

---
+++ Average.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of students: ");
+        int n = sc.nextInt();
+        int[] marks = new int[n];
+        System.out.println("Enter the marks of students: ");
+        for (int i = 0; i < n; i++) {
+            marks[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += marks[i];
+        }
+        double avg = sum / n;
+        System.out.println("Average marks of students: " + avg);
+    }
+}
