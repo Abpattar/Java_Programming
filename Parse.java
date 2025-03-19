ea0cd9bab5f4f41cf5b7f5881a39bc7c1842d791
Abpattar <adityabpattar@gmail.com>
2025-03-19 21:10:55 +0530

    String to int

---
+++ Parse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Parse{
+
+    public static void main(String[] args)
+
+    { 
+        try{
+            System.out.println("Enter Numbers (String) ");
+            Scanner obj = new Scanner(System.in);
+            String str = obj.nextLine();
+            System.out.println("Enter the 2nd Number (String) ");
+            String str2 = obj.nextLine();
+            int num1 = Integer.parseInt();
+            int num2 = Integer.parseInt();
+        }
+    }
+}
