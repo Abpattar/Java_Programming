f19670cf925006a12afb4947db376c3e3be4cdd7
Abpattar <adityabpattar@gmail.com>
2025-03-23 18:10:42 +0530

    Water used by different blocks

---
+++ WaterTank.java
@@ -0,0 +1,24 @@
+public class WaterTank {
+    static int iwater = 1500;
+    
+    static void WaterUsed(String bck , int amt)
+    {
+        System.out.println("Block : "+bck +" Water Used : "+amt +" Litres");
+        iwater = iwater - amt;
+        System.out.println(" Water Remaining : "+iwater);
+
+        if(iwater <= 100)
+        {
+            System.out.println("Low Water Level !!.. Call the Facility Manager to refill the tank");
+            Waterrefill();
+        }
+    }
+    static void Waterrefill()
+    {
+        System.out.println("Water Refilled !!");
+        iwater = 1500 + iwater;
+        System.out.println("Water Level : "+iwater);
+    }
+    
+    
+}
