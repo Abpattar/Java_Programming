3618489b0a15b032fe12e43221ba9696717bff03
Abpattar <adityabpattar@gmail.com>
2025-03-24 19:09:42 +0530

    Using of Constructor

---
+++ Constructor.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Constructor {
+    class Constructor{
+        String name;
+        long rol;
+        double cgpa;
+    }    
+    Constructor()
+    {
+        System.out.println("This is a constructor");
+        System.out.println("Enter Name , Roll Number and CGPA");
+
+    }
+    void input()
+    {
+        Scanner sc = new Scanner(System.in);
+        name = sc.next();
+        rol = sc.nextLong();
+        cgpa = sc.nextDouble();
+        System.out.println(" ");
+    }
+    void display()
+    {
+        System.out.println("Name : " + name);
+        System.out.println("Roll Number : " + rol);
+        System.out.println("CGPA : " + cgpa);
+    }
+    public static void main(String[] args)
+    {
+        Constructor c = new Constructor();
+        c.input();
+        c.display();
+    }
+}
