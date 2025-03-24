8dc753b67b947ead9cb8c90c93da57ee769ce941
Abpattar <adityabpattar@gmail.com>
2025-03-24 18:58:26 +0530

    Employee id

---
+++ Employee.java
@@ -0,0 +1,39 @@
+public class Employee {
+    int empid;
+    String empname;
+    static int empcount = 0;
+    static String companyname = "Shimbala";
+    Employee(int id , String n)
+    {
+        empid = id;
+        empname = n;
+        empcount++;
+    }
+    void display()
+    {
+        System.out.println(empid + " "+ empname + " "+ companyname);
+    }
+    static void printemployee()
+    {
+        System.out.println("Employee  count is " + empcount);
+    }
+    static void changecompanyname()
+    {
+        companyname = "Shimbala Tech";
+    }
+    public static void main(String args[])
+    {
+        Employee e1 = new Employee(1,"Raj");
+        Employee e2 = new Employee(2,"Ravi");
+        Employee e3 = new Employee(3,"Rahul");
+        e1.display();
+        e2.display();
+        e3.display();
+        Employee.printemployee();
+        Employee.changecompanyname();
+        e1.display();
+        e2.display();
+        e3.display();
+    }
+    
+}
