007c7a4601595d053c44ebbf90f47fe0800722c8
Abpattar <adityabpattar@gmail.com>
2025-04-21 19:59:04 +0530

    Single level Inheritance

---
+++ Info_Inheritance(SL).java
@@ -0,0 +1,20 @@
+import java.util*;
+
+class student {
+    String name;
+    int rollno;
+
+    student(String n, int r)
+    {
+        name = n;
+        rollno = r;
+    }
+    void display()
+    {
+        System.out.println("Name : " + name);
+        System.out.println("Roll No : " + rollno);
+    }
+}
+public class Info_Inheritance(SL) {
+    
+}
