52cc4761db5858939578c55d1247ea09822c3646
Abpattar <adityabpattar@gmail.com>
2025-04-21 20:01:38 +0530

    Yeah

--- Info_Inheritance(SL).java
+++ Info_Inheritance(SL).java
@@ -15,6 +15,39 @@ class student {
         System.out.println("Roll No : " + rollno);
     }
 }
+class marks extends student {
+    int m1, m2, m3;
+
+    marks(String n, int r, int a, int b, int c)
+    {
+        super(n, r);
+        m1 = a;
+        m2 = b;
+        m3 = c;
+    }
+    void display()
+    {
+        super.display();
+        System.out.println("Marks 1 : " + m1);
+        System.out.println("Marks 2 : " + m2);
+        System.out.println("Marks 3 : " + m3);
+    }
+}
 public class Info_Inheritance(SL) {
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of the student : ");
+        String n = sc.nextLine();
+        System.out.println("Enter the roll number of the student : ");
+        int r = sc.nextInt();
+        System.out.println("Enter the marks in 3 subjects : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        marks m = new marks(n, r, a, b, c);
+        m.display();
+    }
     
 }
